a4e8fd4d75772f22df10b4da214ca786989ebea9
Karl Fristedt <karl.fristedt@yh.nackademin.se>
2018-08-23 12:35:33 +0200

    Add project files.

---
+++ PersonDataAPI/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonDataAPI.Models
+{
+    public class Person
+    {
+        public string Namn { get; set; }
+        public string Email { get; set; }
+    }
+}
